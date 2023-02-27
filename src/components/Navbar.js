aff59fbce1d09790c10d6277160db7363ba47d1b
dnyana <dnyaneshwarimohod05@gmail.com>
2023-02-27 15:52:29 +0530

    navbar styed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,30 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+
 
 
 const Navbar = ()=>{
     return(
-        <h1>Navbar</h1>
+        <header>
+            <nav>
+                <div className="nav-logo-wrapper">
+                    <h1 className="nav-logo">TakeShape</h1>
+                </div>
+
+                <div className="nav-elements">
+                    <Link className="nav-links" to="#">Home</Link>
+                    <Link className="nav-links" to="#">About</Link>
+                    <Link className="nav-links" to="#">Projects</Link>
+                    <Link className="nav-links" to="#">Contact</Link>
+                </div>
+
+                <div className="menu-wrapper">
+                    <img className="nav-menu-icon" src="/images/menu.png" alt="menu"></img>
+                </div>
+
+            </nav>
+        </header>
     )
 };
 
